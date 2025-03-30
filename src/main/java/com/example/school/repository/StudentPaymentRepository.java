8bd878b48435d16b5074d553e2caf23e05510e3e
Anubhav-verma8055 <159907411+Anubhav-verma8055@users.noreply.github.com>
2025-03-30 18:01:21 +0530

    Create StudentPaymentRepository.java

---
+++ src/main/java/com/example/school/repository/StudentPaymentRepository.java
@@ -0,0 +1,5 @@
+import com.schoolmanagement.model.StudentPayment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentPaymentRepository extends JpaRepository<StudentPayment, Long> {
+}
